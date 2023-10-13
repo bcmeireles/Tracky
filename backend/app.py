7f38df90ec5aabf7eef89ba5eaff4e87e9d9808b
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-10-13 19:53:47 +0100

    Update app.py
    paack route

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.getDetails import ctt, ups
+from routes.getDetails import ctt, ups, paack
 
 app = Flask(__name__)
 CORS(app)
 
 ctt.create_routes(app)
 ups.create_routes(app)
+paack.create_routes(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
