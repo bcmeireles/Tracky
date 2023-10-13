fdde0d6963fd73d457993a4d6b864328940cba53
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-10-13 17:16:46 +0100

    CTT
    Model, route and scraper

--- backend/models/ctt.py
+++ backend/models/ctt.py
@@ -4,18 +4,28 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['tracky']
 
 class CTT:
-    def __init__(self, status):
-        self.status = status # delivered, in-progress, ...
+    def __init__(self, trackingID, ownerUID, status="unverified"):
+        self.trackingID = trackingID
+        self.ownerUID = ownerUID
+        self.status = status
+        self.lastUpdateDate = None
+        self.lastUpdateTime = None
+        self.description = None
+        self.location = None
+        self.reason = None
+        self.receptorName = None
+        self.progress = None
 
     def save(self):
         db['ctt'].insert_one(self.__dict__)
 
-    def update(self):
-        db['ctt'].update_one({'_id': self._id}, {'$set': self.__dict__})
+    @classmethod
+    def update(cls, trackingID, newDetails):
+        db['ctt'].update_one({'trackingID': trackingID}, {'$set': newDetails})
 
     @classmethod
-    def find_by_id(cls, post_id):
-        package_data = db['ctt'].find_one({'_id': post_id})
+    def find_by_id(cls, trackingID):
+        package_data = db['ctt'].find_one({'trackingID': trackingID})
         if package_data:
-            return cls(**package_data)
+            return package_data
         return None
---
+++ backend/routes/getDetails/ctt.py
@@ -0,0 +1,12 @@
+from flask import jsonify, make_response
+from scrapers import ctt
+
+def create_routes(app):
+    @app.route('/getTracking/ctt')
+    def cttTracker():
+        data = ctt.trackCTT()
+        if data['requestStatus'] == 'requestStatus':
+            del data['requestStatus']
+            return make_response(jsonify(data), 200)
+        else:
+            return make_response(jsonify(data), 400)
