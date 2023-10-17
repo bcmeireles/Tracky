14b1e450fc905f5bd3c49420ff5a2674114d621c
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-10-17 17:34:18 +0100

    getTracking fix

--- backend/routes/getTracking/ctt.py
+++ backend/routes/getTracking/ctt.py
@@ -1,10 +1,11 @@
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from scrapers import ctt as cttScraper
 
 def create_routes(app):
-    @app.route('/getTracking/ctt')
+    @app.route('/getTracking/ctt', methods=['GET'])
     def cttTracker():
-        data = cttScraper.trackCTT()
+        trackingID = request.args.get('trackingID')
+        data = cttScraper.trackCTT(trackingID)
         if data['requestStatus'] == 'success':
             del data['requestStatus']
             return make_response(jsonify(data), 200)
--- backend/routes/getTracking/ups.py
+++ backend/routes/getTracking/ups.py
@@ -1,10 +1,11 @@
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from scrapers import ups
 
 def create_routes(app):
-    @app.route('/getTracking/ups')
+    @app.route('/getTracking/ups', methods=['GET'])
     def upsTracker():
-        data = ups.trackUPS()
+        trackingID = request.args.get('trackingID')
+        data = ups.trackUPS(trackingID)
         if data['requestStatus'] == 'success':
             del data['requestStatus']
             return make_response(jsonify(data), 200)
